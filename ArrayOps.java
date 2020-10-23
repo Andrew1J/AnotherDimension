8487c6dd07b60208de6d6121d8a6178512b92ce1
Andrew <ajuang20@stuy.edu>
2020-10-23 13:14:13 -0400

    Added and tested largest method

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,9 +1,16 @@
-public class ArrayOps {
-    public static int sum(int[] arr) {
+public class ArrayOps{
+    public static int sum(int[] arr){
         int sum = 0;
         for(int i=0;i<arr.length;i++){
             sum += arr[i];
         }
         return sum;
+    }
+    public static int largest(int[] arr){
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            max = Math.max(arr[i],max);
+        }
+        return max;
     }
 }
